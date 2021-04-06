d0bc4ada72bf13a6b39537c653113c8cf9d0678a
Savannaha <savwiley@yahoo.com>
2021-04-06 13:17:52 -0500

    created components & catalog

---
+++ src/components/itemPage.js
@@ -0,0 +1,5 @@
+/**see Product Page
+ * 
+ * a list of items can be found in catalog.json
+ * include name, image, price, +/- quantity, cart btn, desc
+ */
---
+++ src/components/navBar.js
@@ -0,0 +1,4 @@
+/**
+ * create fixed top nav bar for every page
+ * includes store name with link to home & # of items in cart
+ */
