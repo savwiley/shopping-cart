38fd113bdcbdd0a108ac8429b2e686c2cfc6ce41
Savannaha <savwiley@yahoo.com>
2021-04-06 13:17:28 -0500

    created routes

---
+++ src/Cart.js
@@ -0,0 +1,8 @@
+/**Cart/Checkout Page
+ * 
+ * includes items already put into cart
+ * each item has delete & increment/decrement btns
+ * total price is displayed (don't worry about tax/shipping)
+ * include btn to actually go to checkout/pay
+ * items must be linked to item pages
+ */
---
+++ src/Product.js
@@ -0,0 +1,5 @@
+/**Product Page
+ * 
+ * a list of items can be found in catalog.json
+ * include name, image, price, +/- quantity, cart btn, desc
+ */
