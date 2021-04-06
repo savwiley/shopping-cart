4ccdfb08fb87e083f2ea2662b620e2e207d409f4
Savannaha <savwiley@yahoo.com>
2021-04-06 12:56:10 -0500

    created base routes file

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "./App.js";
+//import pages
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
