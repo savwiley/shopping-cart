3c57f694bfa24dc41e17b1e165ad0946bd92e5f0
Savannaha <savwiley@yahoo.com>
2021-04-08 13:28:30 -0500

    created navigation div

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,3 +1,34 @@
+import React from "react";
+
+
+const NavBar = () => {
+
+  return (
+    <div className="navBar">
+      <div id="brand">
+        <a href="/">Girl Wonder</a>
+      </div>
+
+      <div id="menu">
+        <a href="url">Apparel</a>
+        <a href="url">Jewelry</a>
+        <a href="url">Cosmetics</a>
+        <a href="url">Checkout</a>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default NavBar;
+
+
+
+
+
+
+
 /**
  * create fixed top nav bar for every page
  * includes store name with link to home & # of items in cart
