a7c66aa2e7ce7b089da24a8f59984855b8d5cee5
Savannaha <savwiley@yahoo.com>
2021-04-09 13:20:37 -0500

    added filter links

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App.js";
+import Apparel from "./Apparel.js";
+import Jewelry from "./Jewelry.js";
+import Cosmetics from "./Cosmetics.js";
 //import pages
 
 const Routes = () => {
@@ -8,6 +11,9 @@ const Routes = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={App} />
+        <Route exact path="/Apparel" component={Apparel} />
+        <Route exact path="/Jewelry" component={Jewelry} />
+        <Route exact path="/Cosmetics" component={Cosmetics} />
       </Switch>
     </BrowserRouter>
   )
