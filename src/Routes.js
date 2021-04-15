6aed705ffaffe2e955fdf40cad363747e539ee33
Savannaha <savwiley@yahoo.com>
2021-04-15 01:34:57 -0500

    added cart logic

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,19 @@ import Cart from "./Cart.js";
 //import pages
 
 const Routes = () => {
+  let list = [];
+
+  const addToCart = (item, qty) => {
+    list.push({
+      product: item[0].product, 
+      price: item[0].price,
+      image: item[0].image,
+      link: item[0].link,
+      quant: qty.current
+    })
+    console.log(list);
+  }
+
   return (
     <BrowserRouter>
       <Switch>
@@ -16,8 +29,12 @@ const Routes = () => {
         <Route exact path="/Apparel" component={Apparel} />
         <Route exact path="/Jewelry" component={Jewelry} />
         <Route exact path="/Cosmetics" component={Cosmetics} />
-        <Route exact path="/Cart" component={Cart} />
-        <Route exact path="/:id" children={<Product />} />
+        <Route exact path="/Cart" render={() => (
+          <Cart list={list} />
+        )} />
+        <Route exact path="/:id" render={() => (
+          <Product addToCart={addToCart} />
+        )} />
       </Switch>
     </BrowserRouter>
   )
@@ -29,4 +46,7 @@ export default Routes;
 /**
  * home page can be App
  * import Cart & Product
+ * 
+ * 
+ * omg i've made a mess
  */
