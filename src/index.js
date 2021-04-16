35d1fbc3a56eb685f7fb0fd2d7673ff51eba0459
Savannaha <savwiley@yahoo.com>
2021-04-16 01:16:14 -0500

    ran prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
 import "./styles/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
