26a0cd9408f6cdca177048ca002e5943e28ce969
Savannaha <savwiley@yahoo.com>
2021-04-16 01:11:30 -0500

    added conditional statements

--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,35 @@
-/**Cart/Checkout Page
- * 
- * includes items already put into cart
- * each item has delete & increment/decrement btns
- * total price is displayed (don't worry about tax/shipping)
- * include btn to actually go to checkout/pay
- * items must be linked to item pages
- */
+import React from "react";
+import NavBar from "./components/navBar.js";
+import Footer from "./components/footer.js";
+import CartItem from "./components/cartItem.js";
+
+const Cart = ({ cart }) => {
+  let total;
+  if (cart.length > 0) {
+    const quantTotals = cart.map((e) => e.price * e.quant);
+    const reducer = (accu, val) => accu + val;
+    total = quantTotals.reduce(reducer);
+  }
+
+  return (
+    <div className="main">
+      <NavBar cart={cart} />
+
+      <div id="product-section" className="subsection">
+        <CartItem cart={cart} />
+      </div>
+
+      <div id="priceTotal">
+        {cart.length > 0
+          ? `Total: $${total.toFixed(2)}`
+          : "No items added to cart."}
+      </div>
+
+      {cart.length > 0 ? <button id="checkoutBtn">Checkout</button> : null}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Cart;
