86b41aef56ebf6027c75d8b1ab0bcfc8d30fe003
Savannaha <savwiley@yahoo.com>
2021-04-16 14:11:59 -0500

    add rm cart item btn

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -2,12 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CartItem = (props) => {
-  const { cart } = props;
+  const { cart, removeFromCart } = props;
+
+  const removeCart = (item) => {
+    const block = document.querySelector(`#${item.image}`);
+    if (block) {
+      block.remove();
+    }
+    removeFromCart(item);
+
+    const totalDiv = document.querySelector("#priceTotal");
+    const total = Number(totalDiv.textContent.slice(8));
+    const itemTotal = item.price * item.quant;
+    const newTotal = (total - itemTotal).toFixed(2);
+    totalDiv.textContent = `Total: $${newTotal}`;
+
+    if (newTotal < 1) {
+      totalDiv.textContent = "No items added to cart.";
+      const checkBtn = document.querySelector("#checkoutBtn");
+      checkBtn.remove();
+    }
+  };
 
   return (
     <div className="products">
       {cart.map((e) => (
-        <div className="cart-block" key={e.image}>
+        <div className="cart-block" key={e.image} id={e.image}>
           <Link to={e.link}>
             <div
               id="cart-image"
@@ -23,6 +43,10 @@ const CartItem = (props) => {
           <span id="cart-name">{e.product}</span>
           <span id="cart-quant">x{e.quant}</span>
           <span id="cart-price">${(e.price * e.quant).toFixed(2)}</span>
+
+          <button id="rmCart" onClick={() => removeCart(e)}>
+            remove from cart
+          </button>
         </div>
       ))}
     </div>
