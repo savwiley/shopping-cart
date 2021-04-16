cfdbb2d21ebd54dec89146b9723203fac598a675
Savannaha <savwiley@yahoo.com>
2021-04-16 01:14:16 -0500

    added credit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-
   return (
     <div className="footer">
-      I'm the footer!
+      <a href="https://github.com/savwiley/shopping-cart">
+        <FontAwesomeIcon icon={faGithubAlt} />
+      </a>
     </div>
-  )
-}
-
-
+  );
+};
 
 export default Footer;
